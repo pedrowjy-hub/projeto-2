fb9fa95bfc9f41f69ea522858e38501c1f38560d
JulioBap <schmidtjulio11@gmail.com>
2026-09-08 09:14:06 -0300

    criando-teste-listar-imoveis-especifico-id-nao-existe

--- test_api.py
+++ test_api.py
@@ -63,3 +63,25 @@ def test_listar_imoveis_com_dados(mock_conectar_banco, client):
     mock_cur.fetchall.assert_called_once()
     mock_cur.close.assert_called_once()
     mock_con.close.assert_called_once()
+
+@patch("api.conectar_banco")
+def test_listar_imoveis_id_nao_existe(mock_conectar_banco, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_cursor.fetchone.return_value = None
+    mock_conectar_banco.return_value = mock_conn
+
+    response = client.get("/imoveis/2000")
+
+    assert response.status_code == 404
+    assert response.get_json() == {"erro": "Imoveis não encontrado"}
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT id, logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE id = ?",
+        (2000,),
+    )
+    mock_cursor.fetchone.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
