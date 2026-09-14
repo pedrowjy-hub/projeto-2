ba2897f5a4d46cf6bbbeca95b901107c8d82f88b
Pedro Wu <pedro.wjy@gmail.com>
2026-09-14 15:48:05 -0300

    Mudei a ordem de fechamento de lista imoveis

--- api.py
+++ api.py
@@ -36,8 +36,8 @@ def obter_imovel(id):
     cursor = conexao.cursor()
     cursor.execute("SELECT id, logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE id = %s", (id,))
     resultado = cursor.fetchone()
-    conexao.close()
     cursor.close()
+    conexao.close()
     if resultado is None:
         return jsonify({"erro": "Imoveis não encontrado"}), 404
     imoveis = {'id': resultado[0], 'logradouro': resultado[1], 'tipo_logradouro': resultado[2], 'bairro': resultado[3], 'cidade': resultado[4], 'cep': resultado[5], 'tipo': resultado[6], 'valor': resultado[7], 'data_aquisicao': resultado[8]}
